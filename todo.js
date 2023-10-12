e536e1010ee7292a6bc737f1a0724823bf4e1324
KNiC.xo <ntcaliwag@hotmail.com>
2023-10-12 04:53:50 -0700

    Can enter tasks and add them to a list

--- todo.js
+++ todo.js
@@ -1,11 +1,34 @@
 const inputField = document.getElementById('inputField');
 const addBtn = document.getElementById('addTask');
-const taskDiv = document.querySelector('.taskDiv');
+const taskUL = document.querySelector('.tasks');
+
+let totalTasks = 0;
 
 addBtn.addEventListener('click', addTask);
 
+function createEventListenters(taskID) {
+  
+}
+
 function addTask() {
+  if (inputField.value == '') {
+    return;
+  }
+
+  totalTasks++;
   const task = inputField.value;
-  console.log(task);
+  const taskElement = document.createElement('li');
+  taskElement.className = `task${totalTasks}`;
+  task.dataset.taskID = totalTasks;
+  taskElement.innerHTML = 
+  `<input class="checkbox${totalTasks}" type="checkbox"> 
+   ${task} 
+   <button class="edit${totalTasks}">Edit</button> 
+   <button class="delete${totalTasks}">Delete</button>`;
+  taskUL.append(taskElement);
+
+  createEventListenters(task.dataset.taskID);
+
+  inputField.value = '';
 }
 
